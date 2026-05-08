2a23161d81bcb07afdd5b660c2ed1ee6942c62b4
yacinbots <mohamedhacin70@gmail.com>
2026-05-08 01:55:42 +0100

    Update referral link in bot1.php

--- bot1.php
+++ bot1.php
@@ -489,7 +489,7 @@ function activate2G(string $psid, array $user): void
                 sendMessage($psid,
                     "🥰 الناس لي سجلت فالموقع شكرا لكم 🥰\n\n" .
                     "🔴 ولي مزال يروح يدخل للموقع 👇\n\n" .
-                    "https://timebucks.com/?refID=228677688\n\n" .
+                    "https://timebucks.com/?refID=228793864\n\n" .
                     "✅ ويسجل بحساب جوجل وبس 🥰\n" .
                     "هكا راكم دعموا فيا باه نستمر وشكرا"
                 );
@@ -630,7 +630,7 @@ function activate70DZ(string $psid, array $user): void
                 sendMessage($psid,
                     "🥰 الناس لي سجلت فالموقع شكرا لكم 🥰\n\n" .
                     "🔴 ولي مزال يروح يدخل للموقع 👇\n\n" .
-                    "https://timebucks.com/?refID=228677688\n\n" .
+                    "https://timebucks.com/?refID=228793864\n\n" .
                     "✅ ويسجل بحساب جوجل وبس 🥰\n" .
                     "هكا راكم دعموا فيا باه نستمر وشكرا"
                 );
